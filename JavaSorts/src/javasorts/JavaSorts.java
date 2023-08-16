847b845537fabb03bb19d5a6b1bf8d1fd2a4dd79
Daiana da Cunha Mendes <15385404608@CAMPUS.MACHADO.MCH>
2023-08-16 10:29:52 -0300

    aula 16/08

--- JavaSorts/src/javasorts/JavaSorts.java
+++ JavaSorts/src/javasorts/JavaSorts.java
@@ -13,23 +13,40 @@ import java.util.Scanner;
  * @author daiana
  */
 public class JavaSorts {
+    static int menuEntrada(){
+        Scanner scanner= new Scanner(System.in);
+        int op;
+        System.out.println("1- Gerar dados");
+        System.out.println("2- Entrar com dados");
+        op = scanner.nextInt();
+        return op;
+    }
+    
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
         Random rand = new Random();
-        int tamanho;
+        int tamanho, op;
          System.out.println("Tamanho do vetor: ");
            tamanho = scanner.nextInt();
         int[] vetor = new int[tamanho];
-         System.out.println("Dados vetor: ");
-         
-         for(int i=0; i<vetor.length; i++)
+        op = menuEntrada();
+        switch(op){
+            case 1:for(int i=0; i<vetor.length; i++)
+             vetor[i] = rand.nextInt(100);
+               break;
+            case 2: System.out.println("Dados vetor: ");
+               for(int i=0; i<vetor.length; i++)
              vetor[i] = scanner.nextInt();
-         //mostrar o vetor
+               break;
+        }// fim switch
+     //mostrar o vetor
          System.out.println("Vetor original: ");
          printArray(vetor);
          
-         //ordenar o vetor
-         BubbleSort.bSortComentado(vetor);
+     //ordenar o vetor
+               //BubbleSort.bSortComentado(vetor);
+               //SelectionSort.sSortComentado(vetor);
+              InsertionSort.iSortComentado(vetor);
          
          //mostrar ordenado
          System.out.println("\nVetor ordenado: ");
