7d8c3a365e8293159d4158de741f4dab6e5e3345
Daiana Mendes <103112318+daianamendesss@users.noreply.github.com>
2023-08-22 08:34:40 -0300

    Add files via upload

---
+++ AtividadeBubbleSort/src/javasorts/JavaApplication4.java
@@ -0,0 +1,33 @@
+package javasorts;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author daiana
+ */
+public class AtividadeBubbleSort {
+    
+    public static void bSort(int array[]) {
+        int n = array.length;
+        int temp;
+        for(int fase = 1; fase < n; fase++) {
+            boolean trocaFase = false;
+
+            for(int comp = 0; comp < n - fase; comp++) {
+                if(array[comp] > array[comp + 1]) {
+                    temp = array[comp];
+                    array[comp] = array[comp + 1];
+                    array[comp + 1] = temp;
+                    trocaFase = true;
+                }
+            }
+
+            if(!trocaFase) {
+                return;
+            }// fim troca
+        }
+    }
+    
+    
+            
